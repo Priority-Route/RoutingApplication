f5553a270b76ec43189587b31e26be77ac54f191
Peejay15 <pjruggirello@gmail.com>
2020-04-01 17:30:01 -0400

    These were not added for some reason.

---
+++ PriorityRoute/ManageUsers.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PriorityRoute
+{
+    public partial class ManageUsers : ContentPage
+    {
+        public ManageUsers()
+        {
+            InitializeComponent();
+        }
+        private async void ManageUsersClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}

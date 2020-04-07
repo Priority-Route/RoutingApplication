cfee336008af24dfca5317fb68f30275bb2938f4
Louis Benoit <louis.benoit@student.fairfield.edu>
2020-04-07 18:20:14 -0400

    Added Login Page with minimal functionality

---
+++ PriorityRoute/LoginPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PriorityRoute
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private async void LoginClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}

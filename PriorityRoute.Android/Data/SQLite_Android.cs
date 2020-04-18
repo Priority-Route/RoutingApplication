74c1b96494170af16a189d8dea2fdd33362fd06a
Peejay15 <pjruggirello@gmail.com>
2020-04-18 14:45:48 -0400

    Added Database

---
+++ PriorityRoute.Android/Data/SQLite_Android.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using PriorityRoute.Data;
+using PriorityRoute.Droid.Data;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+
+namespace PriorityRoute.Droid.Data
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLite_Android() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var sqliteFileName = "TestDB.db3";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFileName);
+            var conn = new SQLite.SQLiteConnection(path);
+
+            return conn;
+        }
+    }
+}
---
+++ PriorityRoute.iOS/Data/SQLite_IOS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using PriorityRoute.Data;
+using PriorityRoute.iOS.Data;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_IOS))]
+
+namespace PriorityRoute.iOS.Data
+{
+    public class SQLite_IOS : ISQLite
+    {
+        public SQLite_IOS() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var fileName = "Testdb.db3";
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var libraryPath = Path.Combine(documentPath, "..", "Library");
+            var path = Path.Combine(libraryPath, fileName);
+            var connection = new SQLite.SQLiteConnection(path);
+            return connection;
+        }
+    }
+}

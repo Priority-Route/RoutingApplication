6b91f4830b4e6abaab4c033eb39f976e3610e5c3
Letters1836 <chakim2017@gmail.com>
2020-05-03 21:20:40 -0400

    push

---
+++ PriorityRoute/TestFiles/DBInsertData.cs
@@ -0,0 +1,74 @@
+// File Name: DBInsertData.cs
+// Purpose: test data update functions in database
+//
+// Author: Christian Hakim
+
+using System.Collections.Generic;
+using PriorityRoute.Data;
+using PriorityRoute.Models;
+
+namespace PriorityRoute.TestFiles
+{
+    public class DBInsertData
+    {
+        public void RunTest()
+        {
+            DBUserOps userOps = new DBUserOps();
+            DBCompanyOps compOps = new DBCompanyOps();
+            DBReceptacleOps recOps = new DBReceptacleOps();
+
+            setUserCompIDs(userOps, compOps);
+            setRecCompIDs(compOps, recOps);
+            setRecCoordinates(compOps, recOps);
+        }
+
+        public static void setUserCompIDs(DBUserOps userOps, DBCompanyOps compOps)
+        {
+            userOps.GetUser("testUser1").CompanyID = compOps.GetCompany("testCompany1").ID;
+            userOps.GetUser("testUser2").CompanyID = compOps.GetCompany("testCompany1").ID;
+            userOps.GetUser("testUser3").CompanyID = compOps.GetCompany("testCompany1").ID;
+            userOps.GetUser("testUser4").CompanyID = compOps.GetCompany("testCompany2").ID;
+            userOps.GetUser("testUser5").CompanyID = compOps.GetCompany("testCompany2").ID;
+            userOps.GetUser("testUser6").CompanyID = compOps.GetCompany("testCompany2").ID;
+            userOps.GetUser("testUser7").CompanyID = compOps.GetCompany("testCompany3").ID;
+            userOps.GetUser("testUser8").CompanyID = compOps.GetCompany("testCompany3").ID;
+        }
+
+        public static void setRecCompIDs(DBCompanyOps compOps, DBReceptacleOps recOps)
+        {
+            recOps.GetReceptacle("testRec1").CompanyID = compOps.GetCompany("testCompany1").ID;
+            recOps.GetReceptacle("testRec2").CompanyID = compOps.GetCompany("testCompany1").ID;
+            recOps.GetReceptacle("testRec3").CompanyID = compOps.GetCompany("testCompany2").ID;
+            recOps.GetReceptacle("testRec4").CompanyID = compOps.GetCompany("testCompany2").ID;
+            recOps.GetReceptacle("testRec5").CompanyID = compOps.GetCompany("testCompany3").ID;
+            recOps.GetReceptacle("testRec6").CompanyID = compOps.GetCompany("testCompany3").ID;
+        }
+
+        public static void setRecCoordinates(DBCompanyOps compOps, DBReceptacleOps recOps)
+        {
+            List<Receptacle> network = recOps.GetNetwork(compOps.GetCompany("testCompany1").ID);
+
+            foreach(Receptacle rec in network)
+            {
+                rec.Latitude = "0.0000";
+                rec.Longitude = "0.0000";
+            }
+
+            List<Receptacle> network = recOps.GetNetwork(compOps.GetCompany("testCompany2").ID);
+
+            foreach(Receptacle rec in network)
+            {
+                rec.Latitude = "0.0000";
+                rec.Longitude = "0.0000";
+            }
+
+            List<Receptacle> network = recOps.GetNetwork(compOps.GetCompany("testCompany3").ID);
+
+            foreach(Receptacle rec in network)
+            {
+                rec.Latitude = "0.0000";
+                rec.Longitude = "0.0000";
+            }
+        }
+    }
+}
---
+++ PriorityRoute/TestFiles/DBInsertObjects.cs
@@ -0,0 +1,101 @@
+// File Name: DBInsertObjects.cs
+// Purpose: test data insert functions in database
+//
+// Author: Christian Hakim
+
+using PriorityRoute.Data;
+using PriorityRoute.Models;
+
+namespace PriorityRoute.TestFiles
+{
+    public class DBInsertObjects
+    {
+        public void RunTest()
+        {
+            DBUserOps userOps = new DBUserOps();
+            DBCompanyOps compOps = new DBCompanyOps();
+            DBReceptacleOps recOps = new DBReceptacleOps();
+
+            addUsers(userOps);
+            addCompanies(compOps);
+            addReceptacles(recOps);
+        }
+
+        private static void addUsers(DBUserOps ops)
+        {
+            User user1 = new User();
+            User user2 = new User();
+            User user3 = new User();
+            User user4 = new User();
+            User user5 = new User();
+            User user6 = new User();
+            User user7 = new User();
+            User user8 = new User();
+            
+            user1.Username = "testUser1";
+            user1.Password = "testPass1";
+            user2.Username = "testUser2";
+            user2.Password = "testPass2";
+            user3.Username = "testUser3";
+            user3.Password = "testPass3";
+            user4.Username = "testUser4";
+            user4.Password = "testPass4";
+            user5.Username = "testUser5";
+            user5.Password = "testPass5";
+            user6.Username = "testUser6";
+            user6.Password = "testPass6";
+            user7.Username = "testUser7";
+            user7.Password = "testPass7";
+            user8.Username = "testUser8";
+            user8.Password = "testPass8";
+            
+            ops.AddUser(user1);
+            ops.AddUser(user2);
+            ops.AddUser(user3);
+            ops.AddUser(user4);
+            ops.AddUser(user5);
+            ops.AddUser(user6);
+            ops.AddUser(user7);
+            ops.AddUser(user8);
+        }
+
+        private static void addCompanies(DBCompanyOps ops)
+        {
+            Company comp1 = new Company();
+            Company comp2 = new Company();
+            Company comp3 = new Company();
+
+            comp1.Name = "testCompany1";
+            comp2.Name = "testCompany2";
+            comp3.Name = "testCompany3";
+
+            ops.AddCompany(comp1);
+            ops.AddCompany(comp2);
+            ops.AddCompany(comp3);
+        }
+
+        private static void addReceptacles(DBReceptacleOps ops)
+        {
+            Receptacle rec1 = new Receptacle();
+            Receptacle rec2 = new Receptacle();
+            Receptacle rec3 = new Receptacle();
+            Receptacle rec4 = new Receptacle();
+            Receptacle rec5 = new Receptacle();
+            Receptacle rec6 = new Receptacle();
+
+            rec1.Name = "testRec1";
+            rec2.Name = "testRec2";
+            rec3.Name = "testRec3";
+            rec4.Name = "testRec4";
+            rec5.Name = "testRec5";
+            rec6.Name = "testRec6";
+
+            ops.AddReceptacle(rec1);
+            ops.AddReceptacle(rec2);
+            ops.AddReceptacle(rec3);
+            ops.AddReceptacle(rec4);
+            ops.AddReceptacle(rec5);
+            ops.AddReceptacle(rec6);
+        }
+    }
+}
